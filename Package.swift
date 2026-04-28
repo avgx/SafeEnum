8878adc9ecfccb3b04f785ae675098c886660fe1
avgx <agovorovsky@gmail.com>
2026-04-28 06:53:01 +0300

    Package.swift: declare platforms and defaultLocalization

--- Package.swift
+++ Package.swift
@@ -5,16 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "SafeEnum",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v15),
+        .macCatalyst(.v15),
+        .macOS(.v13),
+        .watchOS(.v9),
+        .tvOS(.v15),
+        .visionOS(.v1)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SafeEnum",
             targets: ["SafeEnum"]
         ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SafeEnum"
         ),
